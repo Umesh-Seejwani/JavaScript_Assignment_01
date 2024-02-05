6f3544c3f121fb83c3845a9788172eb4bd403cab
Umesh Premjee <130490125+Umesh-Seejwani@users.noreply.github.com>
2024-02-05 14:36:29 +0500

    Completed

---
+++ app.js
@@ -0,0 +1,118 @@
+// JAVASCRIPT ASSIGNMENT
+
+// QUESTION NO 1:
+// alert("Welcome To Our Website");
+
+// QUESTION NO 2:
+// alert(" Error! Please Enter Valid Password");
+
+// QUESTION NO 3:
+// alert(" Hint: Use Line Break");
+
+// QUESTION NO 4:
+// alert("Hello, Welcome to our Website");
+// alert("Hope You Enjoy To Our Website");
+// alert("Thank You For Visisting");
+
+// QUESTION NO 5:
+// QUESTION NO 6:
+
+// QUESTION NO 7:
+// let username = "Umesh";
+
+// QUESTION NO 8:
+// let myName = "Umesh Premjee";
+
+// QUESTION NO 9:
+
+// QUESTION NO 10:
+// let student_1 = 98;
+// let student_2 = 90;
+// let student_3 = 85;
+
+// alert("Student_1 Marks is = " +student_1);
+// alert("Student_2 Marks is = " +student_2);
+// alert("Student_3 Marks is = " +student_3);
+
+// QUESTION NO 11:
+// let email = "umeshpremjee2@gmail.com";
+// alert(email);
+
+// QUESTION NO 12:
+// var str = "“▬▬▬▬▬▬▬▬▬ ஜ ۩۞۩ ஜ ▬▬▬▬▬▬▬▬▬”";
+// document.write(str);
+// QUESTION NO 13:
+
+// QUESTION NO 14:
+// let age = 21;
+// alert("My Age is " +age);
+
+// QUESTION NO 15:
+// let visitors_1 = 5;
+// let visitors_2 = 10;
+
+// alert("Yo Have Visited " +visitors_1+ " Times");
+// alert("Yo Have Visited " +visitors_2+ " Times");
+
+// QUESTION NO 16:
+// let birthYear = 2000;
+// alert("My bIrthYear is "+birthYear);
+
+// QUESTION NO 17:
+// let visitorName = "John Daoe";
+// let product = "T-Shirts";
+// let quantity = 5;
+
+// alert(visitorName+ " Purchase "+ quantity+" " + product);
+
+// QUESTION NO 18:
+// let variable_1 = "hassan", variable_2 = "zohaib", variable_3 = "ahsan";
+// alert(variable_1);
+// alert(variable_2);
+// alert(variable_3);
+
+// QUESTION NO 19:
+// legal Names
+// let username
+// let user_password
+// let firstUserName
+// illegal Names
+// let @myname
+// let information$Data
+
+// QUESTION NO 20:
+
+// QUESTION NO 21:
+// let num1= (prompt("Enter Number1 to add "));
+// let num2=(prompt("Enter Number2 to add "));
+// let sum = num1 + num2;
+
+// console.log(sum);
+
+// QUESTION NO 22:
+// let num1= (prompt("Enter Number1 to add "));
+// let num2=(prompt("Enter Number2 to add "));
+// let mul = num1 * num2;
+
+// console.log(mul);
+
+// let num1= (prompt("Enter Number1 to add "));
+// let num2=(prompt("Enter Number2 to add "));
+// let div = num1 / num2;
+
+// console.log(div);
+
+// let num1= (prompt("Enter Number1 to add "));
+// let num2=(prompt("Enter Number2 to add "));
+// let mod = num1 % num2;
+
+// console.log(mod);
+
+// QUESTION NO 24:
+// let ticket_price = 600;
+
+// let sold = 600 * 5;
+
+// console.log("Cost of 5 tickets sold is "+sold);
+
+
